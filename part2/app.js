c4054fe5310e7cb7e9b4f4b132f7b5725f9ccac3
Samuel Pitkänen <Samppa@Samuels-MacBook-Pro.local>
2018-11-06 15:57:01 +0200

    Part 3

--- part2/app.js
+++ part2/app.js
@@ -9,6 +9,14 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+//Set up mongoose connection
+var mongoose = require('mongoose');
+var mongoDB = 'mongodb://admin:admin1@ds151463.mlab.com:51463/local_library';
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
