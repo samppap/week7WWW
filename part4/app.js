d902b8808d1a1c7dc24736b1346e34f3d52d72ba
Samuel Pitkänen <Samppa@Samuels-MacBook-Pro.local>
2018-11-20 20:38:04 +0200

    part 5

--- part4/app.js
+++ part4/app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var catalogRouter = require('./routes/catalog');  //Import routes for "catalog" area of site
+var catalogRouter = require('./routes/catalog');
 
 var app = express();
 
@@ -30,8 +30,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/catalog', catalogRouter);  // Add catalog routes to middleware chain.
-
+app.use('/catalog', catalogRouter);  
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
